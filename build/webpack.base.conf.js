e5417bf7980ee45a3bdcb8b370d9dd11a2922a43
Ganuelas, Edward (TOR-MCL) <Edward.Ganuelas@maclaren.com>
2017-11-24 14:43:52 -0500

    Add babel-polyfill

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+require('babel-polyfill');
 
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
@@ -11,7 +12,7 @@ function resolve(dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill', './src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
