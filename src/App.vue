bffa13c0967fcf97c9cdfae3d3c4e96176392414
Edward Ganuelas <epganuelas@gmail.com>
2017-12-28 23:47:45 -0500

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
       <main>
         <router-view></router-view>
       </main>
+      <v-footer>
+          <v-container>
+            <v-spacer></v-spacer>
+            <div><p class="text-xs-center"><a href="https://eightrayesun.com">EightRayedSun</a></p></div>
+          </v-container>
+        </v-footer>
     </v-app>
   </div>
 </template>
