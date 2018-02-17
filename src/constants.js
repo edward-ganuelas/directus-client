8fd0acbdcfb141cd543af3cb1461fe0f9566e71d
Edward Paulo Ganuelas <epganuelas@gmail.com>
2018-02-17 16:01:11 -0500

    Feature/filters (#16)
    * Add endpoint for tags
    
    * Golden ratio grid
    
    * Add BlogFilters skeleton and api call
    
    * Caching filters data
    
    * Font update
    
    * Finalizing filters
    
    * Layout fixes

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const API = {
     "post": process.env.APIHOST + '/api/1.1/tables/blog/rows/',
-    "user": process.env.APIHOST + '/api/1.1/users/'
+    "user": process.env.APIHOST + '/api/1.1/users/',
+    "tags": process.env.APIHOST + '/api/1.1/tables/tags/rows/'
 }
