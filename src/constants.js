433559be73915d34315df970b3ac05515d32efb3
Edward Ganuelas <epganuelas@gmail.com>
2018-08-12 14:26:25 -0400

    move to directus js sdk

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const API = {
+    "url": process.env.APIHOST + '/api/1.1',
     "post": process.env.APIHOST + '/api/1.1/tables/blog/rows/',
     "user": process.env.APIHOST + '/api/1.1/users/',
     "tags": process.env.APIHOST + '/api/1.1/tables/tags/rows/'
